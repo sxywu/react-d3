9c07b1f0be1679ddc07482ca3f7021acee606378
Shirley Wu <shirleyxueyangwu@gmail.com>
2015-07-17 00:47:09 -0700

    page styling

--- scripts/components/Page.jsx
+++ scripts/components/Page.jsx
@@ -1,11 +1,33 @@
 var React = require('react');
 
 class PageComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {style: this.calculateDimensions()};
+  }
+  componentWillMount() {
+    window.addEventListener('resize', this._onWindowResize);
+  }
+  _onWindowResize() {
+    var style = this.calculateDimensions();
+    this.setState({style});
+  }
+  calculateDimensions() {
+    var documentWidth = window.innerWidth;
+    var documentHeight = window.innerHeight;
+    var width = documentWidth * .8;
+    var height = documentHeight * .8;
+    var top = (documentHeight - height) / 2;
+    var left = (documentWidth - width) / 2;
+    var margin = top + 'px ' + left + 'px';
+
+    return {width, "min-height": height, margin};
+  }
   render() {
     var title = this.props.title && (<h1>{this.props.title}</h1>);
     var subtitle = this.props.subtitle && (<h2>{this.props.subtitle}</h2>);
     return (
-      <div>
+      <div className="Page" style={this.state.style}>
         {title}
         {subtitle}
         {this.props.children}
