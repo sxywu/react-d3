76d81a961c40580a825f0f1119ea8bd198eafbd7
Shirley Wu <shirleyxueyangwu@gmail.com>
2015-07-27 22:40:11 -0700

    react+d3 section and transition section

--- scripts/components/App.jsx
+++ scripts/components/App.jsx
@@ -1,5 +1,6 @@
 var React = require('react');
 var PageComponent = require('./Page.jsx');
+var SectionComponent = require('./Section.jsx');
 var Label = require('./Label.jsx');
 var ImageComponent = require('./Image.jsx');
 var ColumnComponent = require('./Column.jsx');
@@ -15,8 +16,8 @@ class AppComponent extends React.Component {
         {this.renderIllumination()}
         {this.renderChallenges()}
         {this.renderExpenseApp()}
-        {this.renderIntelligentUpdates()}
-        {this.renderProblem()}
+        {this.renderReactD3()}
+        {this.renderTransition()}
       </div>
     );
   }
@@ -42,7 +43,7 @@ class AppComponent extends React.Component {
     return (
       <PageComponent subtitle="Illumio's Illumination: How I Got Here">
         <ColumnComponent>
-          <ImageComponent src="images/illumination.png" imageWidth={400} />
+          <ImageComponent src="images/illumination.png" />
           <ColumnChildComponent>
             <h3><a href="https://www.illumio.com/home" target="_new">Illumio</a></h3>
             <p>
@@ -55,10 +56,10 @@ class AppComponent extends React.Component {
           </ColumnChildComponent>
         </ColumnComponent>
         <ColumnComponent>
-          <ImageComponent src="images/roles.gif" imageWidth={400} contentType="caption">
+          <ImageComponent src="images/roles.gif" contentType="caption">
             <p>Assign roles to workloads within application</p>
           </ImageComponent>
-          <ImageComponent src="images/rules.gif" imageWidth={400} contentType="caption">
+          <ImageComponent src="images/rules.gif" contentType="caption">
             <p>Create rules between roles for specific or all services</p>
           </ImageComponent>
         </ColumnComponent>
@@ -96,67 +97,103 @@ class AppComponent extends React.Component {
       </PageComponent>
     );
   }
-  renderIntelligentUpdates() {
+  renderReactD3() {
     return (
       <PageComponent subtitle="React &amp; D3">
-        <h3><Label>1</Label>  Intelligent Updates</h3>
-        <ColumnComponent>
-          <ColumnChildComponent>
-            <h3><a href="https://facebook.github.io/react/" target="_new">React</a></h3>
-            <p>
-              A library for developing single-page apps whose data change over time.
-            </p>
-            <p>
-              User specifies component structure with <a href="https://facebook.github.io/react/docs/component-specs.html#render" target="_new">render</a>, and operates on component at different points of the component <a href="https://facebook.github.io/react/docs/component-specs.html#lifecycle-methods">lifecycle</a>.<br />
-              React then uses <CodeComponent inline={true}>render</CodeComponent> output to calculate minimum updates in DOM between data changes.
-            </p>
-          </ColumnChildComponent>
-          <ColumnChildComponent>
-            <h3><a href="http://www.d3js.org" target="_new">D3</a></h3>
-            <p>
-              A library for developing interactive visualizations.
-            </p>
-            <p>
-              User binds data to a selection, and uses <a href="https://github.com/mbostock/d3/wiki/Selections#enter" target="_new">enter</a> to append new elements to DOM, and <a href="https://github.com/mbostock/d3/wiki/Selections#exit">exit</a> to remove elements no longer in the new dataset from DOM.
-            </p>
-            
-          </ColumnChildComponent>
-        </ColumnComponent>
 
-        <h3><Label>2</Label>  Problem</h3>
-        <ColumnComponent>
-          <ColumnChildComponent>
-            <p>
-              As part of the component lifecycle, React renders directly to the DOM.  D3 uses a combination of <a href="https://github.com/mbostock/d3/wiki/Selections#selecting-elements" target="_new">selections</a> and <a href="https://github.com/mbostock/d3/wiki/Selections#data" target="_new">data-binding</a> to calculate what should be in the DOM.
-            </p>
-            <p>
-              <strong>Both require access to the DOM.</strong>
-            </p>
-          </ColumnChildComponent>
-        </ColumnComponent>
-        <ColumnComponent>
-          <ColumnChildComponent>
-            <CodeComponent path="scripts/code/react-render.txt" />
-            <CaptionComponent>
-              From <a href="https://github.com/sxywu/expenses/blob/master/scripts/components/Expense.jsx" target="_new">components/Expense.jsx</a> and <a href="https://github.com/sxywu/expenses/blob/master/scripts/components/Graph.jsx" target="_new">components/Graph.jsx</a>
-            </CaptionComponent>
-          </ColumnChildComponent>
-          <ColumnChildComponent>
-            <CodeComponent path="scripts/code/d3-enter-update-exit.txt" />
-          </ColumnChildComponent>
-        </ColumnComponent>
+        <SectionComponent position="first">
+          <h3><Label>1</Label>  Intelligent Updates</h3>
+          <ColumnComponent>
+            <ColumnChildComponent>
+              <h3><a href="https://facebook.github.io/react/" target="_new">React</a></h3>
+              <p>
+                A library for developing single-page apps whose data change over time.
+              </p>
+              <p>
+                User specifies component structure with <a href="https://facebook.github.io/react/docs/component-specs.html#render" target="_new">render</a>, and operates on component at different points of the component <a href="https://facebook.github.io/react/docs/component-specs.html#lifecycle-methods">lifecycle</a>.<br />
+                React then uses <CodeComponent inline={true}>render</CodeComponent> output to calculate minimum updates in DOM between data changes.
+              </p>
+            </ColumnChildComponent>
+            <ColumnChildComponent>
+              <h3><a href="http://www.d3js.org" target="_new">D3</a></h3>
+              <p>
+                A library for developing interactive visualizations.
+              </p>
+              <p>
+                User binds data to a selection, and uses <a href="https://github.com/mbostock/d3/wiki/Selections#enter" target="_new">enter</a> to append new elements to DOM, and <a href="https://github.com/mbostock/d3/wiki/Selections#exit">exit</a> to remove elements no longer in the new dataset from DOM.
+              </p>
+            </ColumnChildComponent>
+          </ColumnComponent>
+          <ColumnComponent>
+            <ColumnChildComponent>
+              <CodeComponent path="scripts/code/react-render.txt" />
+              <CaptionComponent>
+                From <a href="https://github.com/sxywu/expenses/blob/master/scripts/components/Expense.jsx" target="_new">components/Expense.jsx</a> and <a href="https://github.com/sxywu/expenses/blob/master/scripts/components/Graph.jsx" target="_new">components/Graph.jsx</a>
+              </CaptionComponent>
+            </ColumnChildComponent>
+            <ColumnChildComponent>
+              <CodeComponent path="scripts/code/d3-enter-update-exit.txt" />
+            </ColumnChildComponent>
+          </ColumnComponent>
+        </SectionComponent>
+
+        <SectionComponent>
+          <h3><Label>2</Label>  Problem</h3>
+          <ColumnComponent>
+            <ColumnChildComponent>
+              <p>
+                As part of the component lifecycle, React renders directly to the DOM.  Certain subsets of D3 (<a href="https://github.com/mbostock/d3/wiki/Transitions" target="_new">transitions</a>, <a href="https://github.com/mbostock/d3/wiki/SVG-Axes" target="_new">axes</a>, etc.) also render directly to the DOM.
+              </p>
+              <p>
+                <strong>Both want control of the DOM, so how do we divide responsibility?</strong>
+              </p>
+            </ColumnChildComponent>
+          </ColumnComponent>
+        </SectionComponent>
+
+        <SectionComponent position="last">
+          <h3><Label>3</Label>  Solution</h3>
+          <ColumnComponent>
+            <ColumnChildComponent>
+              <p>
+                <strong>React manages the structure, D3 manages the attributes.</strong>
+              </p>
+              <CodeComponent path="scripts/code/svg-structure.txt" />
+            </ColumnChildComponent>
+          </ColumnComponent>
+        </SectionComponent>
+
       </PageComponent>
     );
   }
-  renderProblem() {
+  renderTransition() {
     return (
-      <PageComponent subtitle="React &amp; D3: Problem and Solution">
+      <PageComponent subtitle="Transitions">
+        <ColumnComponent>
+          <ImageComponent src="images/transition.gif" imageWidth={500} />
+        </ColumnComponent>
         <ColumnComponent>
           <ColumnChildComponent>
-            <h3>Problem</h3>
             <p>
-
+              Call <CodeComponent inline={true}>ExpenseVisualization.enter</CodeComponent> from <CodeComponent inline={true}>ExpenseComponent.componentDidMount</CodeComponent>: As soon as React inserts the element into the DOM, D3 sets the starting attributes.
             </p>
+            <p>
+              Call <CodeComponent inline={true}>ExpenseVisualization.update</CodeComponent> from <CodeComponent inline={true}>ExpenseComponent.componentDidUpdate</CodeComponent>: When any part of the data changes, D3 transitions the element to its next set of attributes.
+            </p>
+          </ColumnChildComponent>
+        </ColumnComponent>
+        <ColumnComponent>
+          <ColumnChildComponent>
+            <CodeComponent path="scripts/code/d3-transition.txt" />
+            <CaptionComponent>
+              From <a href="https://github.com/sxywu/expenses/blob/master/scripts/visualizations/Expense.js" target="_new">visualizations/Expense.js</a>
+            </CaptionComponent>
+          </ColumnChildComponent>
+          <ColumnChildComponent>
+            <CodeComponent path="scripts/code/react-transition.txt" />
+            <CaptionComponent>
+              From <a href="https://github.com/sxywu/expenses/blob/master/scripts/components/Expense.jsx" target="_new">components/Expense.jsx</a>
+            </CaptionComponent>
           </ColumnChildComponent>
         </ColumnComponent>
       </PageComponent>
